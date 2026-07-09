fdb282072f945719797f09bedd900ff7a069c165
Tonko Mulder <tonko@tonkomulder.nl>
2026-07-09 16:17:40 +0200

    add models/migrations according to specs

---
+++ app/Models/Contact.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * @property int $id
+ * @property int $company_id
+ * @property string $name
+ * @property string|null $role
+ * @property string|null $email
+ * @property string|null $phone
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property-read Company $company
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, Interaction> $interactions
+ */
+final class Contact extends Model
+{
+    protected $fillable = [
+        'company_id',
+        'name',
+        'role',
+        'email',
+        'phone',
+    ];
+
+    /** @phpstan-return BelongsTo<Company, $this> */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    /** @phpstan-return HasMany<Interaction, $this> */
+    public function interactions(): HasMany
+    {
+        return $this->hasMany(Interaction::class);
+    }
+}
