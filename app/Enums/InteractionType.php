d9faf9cf230abc9f3014f1ce8ab6ac71f8f94aff
Tonko Mulder <tonko@tonkomulder.nl>
2026-07-09 15:12:20 +0200

    add enums ApplicationStatus and InteractionType

---
+++ app/Enums/InteractionType.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum InteractionType: string
+{
+    case NOTE = 'note';
+    case STATUS_CHANGE  = 'status_change';
+    case EMAIL = 'email';
+    case CALL = 'call';
+    case INTERVIEW = 'interview';
+}
